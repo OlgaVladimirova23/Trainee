b9dc20dfd973553b1636d07d1dd25ec1c9d2ff5e
Olga <olga@ubuntu.ubuntu-domain>
2013-09-02 10:35:26 +0400

    init commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+	def new
+    @user = User.new(params[:user])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    @user.save
+    redirect_to @user
+  end
+
+  def show
+     @user = User.find(params[:id])
+   
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trainee::Application.config.secret_key_base = '1a7f51191b05378592eac12ed3aa8b1c4c41a224950c81fa623e77db290aa36274e3d8fe360d13e7483dafc3220e53738fcc8ac4c9cf449118463c81b814d040'
